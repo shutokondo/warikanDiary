63966ba99ffbed78668dffb046fb3d3c3f1cbf3c
根東秀都 <shuto8201@gmail.com>
2015-10-15 19:11:28 +0900

    create Group and GroupManager model

---
+++ warikanDiary/warikanDiary/GroupManager.swift
@@ -0,0 +1,13 @@
+//
+//  GroupManager.swift
+//  warikanDiary
+//
+//  Created by 根東秀都 on 2015/10/15.
+//  Copyright © 2015年 shuto kondo. All rights reserved.
+//
+
+import UIKit
+
+class GroupManager: NSObject {
+
+}
