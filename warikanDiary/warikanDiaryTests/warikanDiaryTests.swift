2f0f05625dee69e36087ccd97db980256978b891
根東秀都 <shuto8201@gmail.com>
2015-10-15 17:56:28 +0900

    create swift app

---
+++ warikanDiary/warikanDiaryTests/warikanDiaryTests.swift
@@ -0,0 +1,36 @@
+//
+//  warikanDiaryTests.swift
+//  warikanDiaryTests
+//
+//  Created by 根東秀都 on 2015/10/15.
+//  Copyright © 2015年 shuto kondo. All rights reserved.
+//
+
+import XCTest
+@testable import warikanDiary
+
+class warikanDiaryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
