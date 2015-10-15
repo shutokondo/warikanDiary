ca907b5f6bb7dd8249a34606abfc6a4eba239afc
根東秀都 <shuto8201@gmail.com>
2015-10-15 18:22:59 +0900

    create cell files with topTableViewController

---
+++ warikanDiary/topTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  topTableViewCell.swift
+//  warikanDiary
+//
+//  Created by 根東秀都 on 2015/10/15.
+//  Copyright © 2015年 shuto kondo. All rights reserved.
+//
+
+import UIKit
+
+class topTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
