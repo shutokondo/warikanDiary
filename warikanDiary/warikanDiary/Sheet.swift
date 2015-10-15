18e75a0aed61323e39f71785f148a1cc24b9d24b
根東秀都 <shuto8201@gmail.com>
2015-10-15 19:11:37 +0900

    create sheet model

---
+++ warikanDiary/warikanDiary/Sheet.swift
@@ -0,0 +1,13 @@
+//
+//  Sheet.swift
+//  warikanDiary
+//
+//  Created by 根東秀都 on 2015/10/15.
+//  Copyright © 2015年 shuto kondo. All rights reserved.
+//
+
+import UIKit
+
+class Sheet: NSObject {
+
+}
