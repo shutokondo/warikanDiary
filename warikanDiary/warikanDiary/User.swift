3872e87acaae9d48c381153400dd43873d98869e
根東秀都 <shuto8201@gmail.com>
2015-10-15 19:11:43 +0900

    create user model

---
+++ warikanDiary/warikanDiary/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  warikanDiary
+//
+//  Created by 根東秀都 on 2015/10/15.
+//  Copyright © 2015年 shuto kondo. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
