f41ceea0f202f9d0fc37cabfdabb9d05bbceb38b
根東秀都 <shuto8201@gmail.com>
2015-10-15 18:23:38 +0900

    create addGroupViewController

---
+++ warikanDiary/warikanDiary/addGroupViewController.swift
@@ -0,0 +1,48 @@
+//
+//  addGroupViewController.swift
+//  warikanDiary
+//
+//  Created by 根東秀都 on 2015/10/15.
+//  Copyright © 2015年 shuto kondo. All rights reserved.
+//
+
+import UIKit
+
+class addGroupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "閉じる", style: .Plain, target: self, action: "close")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "作成", style: .Plain, target: self, action: "create")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func close() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func create() {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
