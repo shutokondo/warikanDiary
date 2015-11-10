1b88d766b41f069553e0eab31582e782789f0944
根東秀都 <shuto8201@gmail.com>
2015-11-10 23:03:23 +0900

    set property num

--- warikanDiary/warikanDiary/Group.swift
+++ warikanDiary/warikanDiary/Group.swift
@@ -11,4 +11,5 @@ import UIKit
 class Group: NSObject {
     var name: String!
     var descript: String!
+    var num: Int!
 }
---
+++ warikanDiary/warikanDiary/UserManager.swift
@@ -0,0 +1,13 @@
+//
+//  UserManager.swift
+//  warikanDiary
+//
+//  Created by 根東秀都 on 2015/10/19.
+//  Copyright © 2015年 shuto kondo. All rights reserved.
+//
+
+import UIKit
+
+class UserManager: NSObject {
+
+}
